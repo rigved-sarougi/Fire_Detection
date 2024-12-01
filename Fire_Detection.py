13b5ac767acb0efada39f8df2e5fe2d40e2a5802
rigved-sarougi <dataanalyst@biolume.in>
2024-12-01 10:16:43 +0530

    Update Fire_Detection.py

--- Fire_Detection.py
+++ Fire_Detection.py
@@ -115,29 +115,3 @@ if app_mode == 'Run on Video':
         text.write(f"<h1 style='text-align: center; color:red;'>{length}</h1>",unsafe_allow_html = True)
         stframe.image(output)
         
-if app_mode == 'Run on WebCam':
-    st.subheader("Detected Fire:")
-    text = st.markdown("")
-    
-    st.sidebar.markdown("---")
-    
-    st.subheader("Output")
-    stframe = st.empty()
-    
-    run = st.sidebar.button("Start")
-    stop = st.sidebar.button("Stop")
-    st.sidebar.markdown("---")
-    
-    cam = cv2.VideoCapture(0)
-    if(run):
-        while(True):
-            if(stop):
-                break
-            ret,frame = cam.read()
-            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            model = load_model()
-            results = model(frame)
-            length = len(results.xyxy[0])
-            output = np.squeeze(results.render())
-            text.write(f"<h1 style='text-align: center; color:red;'>{length}</h1>",unsafe_allow_html = True)
-            stframe.image(output)
